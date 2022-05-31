251dcc629002694d962a0178a451949af5f16b93
Oxana <oxanacode@gmail.com>
2022-05-31 20:05:22 +0300

    refactor: change class name according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
     
-    listItem.className = "list__item task";
+    listItem.className = "task__list-item task";
 
     label.innerText=taskString;
     label.className='task__content';
