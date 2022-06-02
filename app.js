a5f12acd1719e7b61798947bba864e13734a2019
Oxana <oxanacode@gmail.com>
2022-06-02 14:43:27 +0300

    refactor: change class name and selectors according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className = "task__btn delete";
     deleteButtonImg.src = "./remove.svg";
-    deleteButtonImg.className = "btn__img";
+    deleteButtonImg.className = "task__btn-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
